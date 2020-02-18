a63a1e052297d0187e45e88f734c269104f3ed26
prem5038 <prem5038@gmail.com>
2020-02-18 10:10:38 +0530

    Rules table modified

--- src/modules/layout/rule-config-layout/rule-config-layout.component.ts
+++ src/modules/layout/rule-config-layout/rule-config-layout.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RuleConfigLayoutComponent implements OnInit {
 
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns = ['name', 'source', 'status'];
   dataSource = ELEMENT_DATA;
 
   constructor() { }
@@ -19,20 +19,16 @@ export class RuleConfigLayoutComponent implements OnInit {
 
 export interface PeriodicElement {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  //position: number;
+  source: string;
+  status: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {name: 'utif/rules/common/entry-routing-rule.drl', source: 'XENIA', status: 'C'},
+  {name: 'utif/rules/fiss/fiss-trade-routing.drl', source: 'FISS', status: 'M'},
+  {name: 'utif/rules/gos/gos-trade-routing.drl', source: 'GOS', status: 'M'},
+  {name: 'utif/rules/booker/settlement-routing.drl', source: 'BOOKER', status: 'M'},
+  {name: 'utif/rules/booker/eod-msg-routing.drl', source: 'BOOKER', status: 'M'},
+  {name: 'utif/rules/booker/journal-routing.drl', source: 'BOOKER', status: 'M'}  
 ];
